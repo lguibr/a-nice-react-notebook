8f6d1c4a436eb283c3741e956365675083eaac2d
Luís Guilherme Pelin Martins <lgpelin92@gmail.com>
2018-11-25 14:01:48 -0200

    FEA : change colors and add some example text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from "./components/navbar/NavBar.js"
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <NavBar />
       </div>
     );
   }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import './NavBar.css';
+class NavBar extends Component {
+    render() {
+        return (
+            <div className="NavBar">
+                <h1>Eu sou um componente chamado NavBar</h1>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
